854a3d64bbfd968ee4cdedffb155c6ab379240ed
duong nguyen <duongnnse04913@fpt.edu.vn>
2020-04-25 00:31:49 +0700

    sua lai cho giong ban model hien tai

---
+++ src/main/java/model/GaParameter.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.Data;
+
+
+@Data
+public class GaParameter {
+    private int populationSize;
+    private double mutationRate;
+    private double tournamentSize;
+    private Cofficient cofficient;
+    private int convergenceCheckRange;
+}
---
+++ src/main/java/model/Train.java
@@ -0,0 +1,43 @@
+package model;
+
+import data.DataReader;
+import data.Schedule;
+import data.Visualize;
+
+public class Train {
+    public static final int M = 5; //teacher size
+    public static final int K = 20; //subject size
+    public static final int N = 37; //class size
+
+    public Schedule scheduleFrame;
+    public Visualize visualize;
+
+    public Train() {
+        this.scheduleFrame = new Schedule();
+        this.visualize = new Visualize("GA visualization", "Without Minimizing standard deviation");
+    }
+
+    public void notify(Chromosome c, double bestFitness, double avgFitness, int violation) {
+        scheduleFrame.addSchedule(c);
+        this.visualize.add(bestFitness, avgFitness, violation);
+    }
+
+    public static void main(String[] args) {
+
+//        Model model = DataGenerator.Gendata(M, K, N);
+
+        Train train = new Train();
+        Model model = DataReader.getData();
+        System.out.println(model.getTeachers().size());
+        System.out.println(model.getSubjects().size());
+        System.out.println(model.getClasses().size());
+
+//        for(Subject subject:model.getSubjects()) {
+//            System.out.println(subject.getName());
+//        }
+
+        GeneticAlgorithm ga = new GeneticAlgorithm(model, train);
+        ga.start();
+    }
+}
+
